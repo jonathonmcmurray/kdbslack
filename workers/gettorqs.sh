11713987105b02be5be7a51ceb22079edda42aba
jonathonmcmurray <jonathon.mcmurray@aquaq.co.uk>
2018-04-10 12:59:39 +0100

    Change status report to filter only be q torq process

--- workers/gettorqs.sh
+++ workers/gettorqs.sh
@@ -1,6 +1,6 @@
 # output from each line is piped to next line, equivalent to one liner above
 ps -eo user:12,stime,args |    # get all running processes, output user name up to 12 chars
-command grep torq         |    # find those that are torq processes (use "command" to bypass grep alias)
+command grep 'q*torq.q'   |    # find those that are torq processes (use "command" to bypass grep alias)
 awk '{                                         # -- START AWK PROGRAM --
       if($3!="grep"&&$3!="bash"&&$3!="sh")     # ignore the grep process & bash/sh processes
       {printf "%-20s", $1;                     # output the username from each line
